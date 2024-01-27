f769b5c22f44538df5b9b50254dcb82037e2687c
Omer Šabić <omer@datalinx.si>
2024-01-27 13:42:27 +0100

    feat: implement module helper class

---
+++ src/Module.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Eclipse\ModuleTemplate;
+
+use Eclipse\Core\Foundation\Module\AbstractModule;
+
+class Module extends AbstractModule
+{
+    public static string $vendor_name = 'eclipseapp';
+
+    public static string $package_name = 'module-template';
+}
